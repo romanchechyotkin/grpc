ac5697f7cb738a9ea785beddda9e5fefaef742cf
chechyotka <romanchechyotkin@gmail.com>
2023-10-05 21:15:02 +0300

    refactored

--- client/main.go
+++ client/main.go
@@ -38,6 +38,7 @@ func main() {
 	fmt.Println(order)
 
 	// Потоковый RPC на стороне сервера
+	log.Println("server streaming")
 	searchingStream, err := client.SearchOrders(ctx, &wrapperspb.StringValue{Value: "Orange"})
 	for {
 		streamOrder, err := searchingStream.Recv()
@@ -47,6 +48,7 @@ func main() {
 		log.Println("finding order: ", streamOrder)
 	}
 
+	log.Println("client streaming")
 	// Потоковый RPC на стороне клиента
 	updateStream, err := client.UpdateOrders(ctx)
 	if err != nil {
@@ -72,4 +74,49 @@ func main() {
 
 	// Двунаправленный потоковый RPC
 
+}
+
+func orderUnaryClientInterceptor(
+	ctx context.Context, method string, req, reply interface{},
+	cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	log.Println("method", method)
+
+	err := invoker(ctx, method, req, reply, cc, opts...)
+
+	log.Println(reply)
+	return err
+}
+
+func orderStreamClientInterceptor(ctx context.Context, desc *grpc.StreamDesc,
+	cc *grpc.ClientConn, method string,
+	streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	log.Println("======= [Client Interceptor] ", method)
+	s, err := streamer(ctx, desc, cc, method, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return newWrappedStream(s), nil
+}
+
+type wrappedStream struct {
+	grpc.ClientStream
+}
+
+func newWrappedStream(s grpc.ClientStream) grpc.ClientStream {
+	return &wrappedStream{s}
+}
+
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	log.Printf("====== [Client Stream Interceptor] "+
+		"Receive a message (Type: %T) at %v",
+		m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.RecvMsg(m)
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	log.Printf("====== [Client Stream Interceptor] "+
+		"Send a message (Type: %T) at %v",
+		m, time.Now().Format(time.RFC3339))
+	return w.ClientStream.SendMsg(m)
 }
--- server/main.go
+++ server/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"strings"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	pb "server/ecommerce"
 )
@@ -13,6 +20,72 @@ const PORT = ":5000"
 const NETWORK = "tcp"
 
 var combinedShipmentMap map[string]pb.CombinedShipment
+
+type server struct {
+	orderMap map[string]*pb.Order
+}
+
+func (s *server) GetOrder(ctx context.Context, in *wrapperspb.StringValue) (*pb.Order, error) {
+	order, exists := s.orderMap[in.Value]
+	if exists {
+		return order, nil
+	}
+	return nil, errors.New("no such order")
+}
+
+func (s *server) SearchOrders(searchQuery *wrapperspb.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
+	log.Println("searching for", searchQuery.Value)
+	for k, order := range s.orderMap {
+		log.Println(k, order)
+
+		for _, item := range order.Items {
+			log.Println(item)
+
+			if strings.Contains(item, searchQuery.Value) {
+				err := stream.Send(order)
+				if err != nil {
+					return fmt.Errorf("error sending message to stream : %v", err)
+				}
+				log.Print("Matching Order Found : " + k)
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (s *server) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) error {
+	ordersStr := "Updated Order IDs : "
+
+	for {
+		order, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&wrapperspb.StringValue{Value: "orders processed. " + ordersStr})
+		}
+		s.orderMap[order.Id] = order
+		log.Println("Order ID", order.Id, "Updated")
+		ordersStr += order.Id + ", "
+	}
+
+}
+
+func (s *server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
+
+	for {
+		orderId, err := stream.Recv()
+		fmt.Println(orderId)
+		if err == io.EOF {
+			for _, comb := range combinedShipmentMap {
+				stream.Send(&comb)
+			}
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+}
 
 func main() {
 	var srv = &server{
@@ -68,4 +141,48 @@ func main() {
 	if err = s.Serve(listen); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func orderUnaryServerInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+	log.Println("------------------ [Server Interceptor]", info.FullMethod)
+
+	m, err := handler(ctx, req)
+
+	log.Printf(" Post Proc Message : %s", m)
+	return m, err
+}
+
+type wrappedStream struct {
+	grpc.ServerStream
+}
+
+func (w *wrappedStream) RecvMsg(m interface{}) error {
+	log.Printf("====== [Server Stream Interceptor Wrapper] "+
+		"Receive a message (Type: %T) at %s",
+		m, time.Now().Format(time.RFC3339))
+	return w.ServerStream.RecvMsg(m)
+}
+
+func (w *wrappedStream) SendMsg(m interface{}) error {
+	log.Printf("====== [Server Stream Interceptor Wrapper] "+
+		"Send a message (Type: %T) at %v",
+		m, time.Now().Format(time.RFC3339))
+	return w.ServerStream.SendMsg(m)
+}
+
+func newWrappedStream(s grpc.ServerStream) grpc.ServerStream {
+	return &wrappedStream{s}
+}
+
+func orderStreamServerInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Println("====== [Server Stream Interceptor] ", info.FullMethod)
+	err := handler(srv, newWrappedStream(ss))
+	if err != nil {
+		log.Printf("RPC failed with error %v", err)
+	}
+	return err
 }
